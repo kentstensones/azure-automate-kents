c4df428d4543fd2021ea1705e132bed54c37b83e
kentstensones <mdksn23@gmail.com>
2022-02-03 08:46:20 +0100

    Oppgave 4 utført

---
+++ src/oppgave_4.ps1
@@ -0,0 +1,31 @@
+[CmdletBinding()]
+param (
+    [Parameter(HelpMessage = "URL til kortstokk", Mandatory = $false)]
+    [string]
+    $UrlKortstokk = 'http://nav-deckofcards.herokuapp.com/shuffle'
+)
+
+$ErrorActionPreference = 'Stop'
+
+$webRequest = Invoke-WebRequest -Uri $UrlKortstokk
+
+$kortstokkJson = $webRequest.Content
+
+$kortstokk = ConvertFrom-Json -InputObject $kortstokkJson
+
+
+function kortstokkTilStreng {
+    [OutputType([string])]
+    param (
+        [object[]]
+        $kortstokk
+    )
+    $streng = ''
+    foreach ($kort in $kortstokk) {
+        $streng = $streng + "$($kort.suit[0])" + "$($kort.value)" + ","
+    }
+        return $streng.TrimEnd(",")
+}
+
+
+Write-Output "Kortstokk: $(kortStokkTilStreng -kortstokk $kortstokk)" 
