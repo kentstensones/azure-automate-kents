7c344470589b56fafe1a3f39e44a736e5c093a1b
kentstensones <mdksn23@gmail.com>
2022-02-03 09:55:59 +0100

    Oppgave 6 utført

---
+++ src/oppgave_6.ps1
@@ -0,0 +1,58 @@
+[CmdletBinding()]
+param (
+    [Parameter(HelpMessage = "URL til kortstokk", Mandatory = $false)]
+    [string]
+    $UrlKortstokk = 'http://nav-deckofcards.herokuapp.com/shuffle'
+)
+
+$ErrorActionPreference = 'Stop'
+
+$webRequest = Invoke-WebRequest -Uri $UrlKortstokk
+
+$kortstokkJson = $webRequest.Content
+
+$kortstokk = ConvertFrom-Json -InputObject $kortstokkJson
+
+function sumPoengKortstokk {
+    [OutputType([int])]
+    param (
+        [object[]]
+        $kortstokk
+    )
+
+    $poengKortstokk = 0
+
+    foreach ($kort in $kortstokk) {
+        $poengKortstokk += switch ($kort.value) {
+            { $_ -cin @('J', 'Q', 'K') } { 10 }
+            'A' { 11 }
+            default { $kort.value }
+        }
+    }
+    return $poengKortstokk
+}
+function kortstokkTilStreng {
+    [OutputType([string])]
+    param (
+        [object[]]
+        $kortstokk
+    )
+    $streng = ''
+    foreach ($kort in $kortstokk) {
+        $streng = $streng + "$($kort.suit[0])" + "$($kort.value)" + ","
+    }
+        return $streng.TrimEnd(",")
+}
+
+Write-Output "Kortstokk: $(kortStokkTilStreng -kortstokk $kortstokk)" 
+Write-Output "Poengsum: $(sumPoengKortstokk -kortstokk $kortstokk)"
+
+$meg = $kortstokk[0..1]
+$kortstokk = $kortstokk[2..$kortstokk.Count]
+
+$magnus = $kortstokk[0..1]
+$kortstokk = $kortstokk[2..$kortstokk.Count]
+
+Write-Output "Meg: $(kortStokkTilStreng -kortstokk $meg)"
+Write-Output "Magnus: $(kortStokkTilStreng -kortstokk $magnus)"
+Write-Output "Kortstokk: $(kortStokkTilStreng -kortstokk $kortstokk)"
